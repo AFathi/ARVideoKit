580235926bd5581e4055899aa1d9566ed22d841d
AFathi <me@ahmedbekhit.com>
2017-11-05 20:42:30 -0500

    v. 1.02

---
+++ ARVideoKitLib/ARVideoKitLib.h
@@ -0,0 +1,19 @@
+//
+//  ARVideoKitLib.h
+//  ARVideoKitLib
+//
+//  Created by Ahmed Bekhit on 11/5/17.
+//  Copyright © 2017 Ahmed Fathi Bekhit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARVideoKitLib.
+FOUNDATION_EXPORT double ARVideoKitLibVersionNumber;
+
+//! Project version string for ARVideoKitLib.
+FOUNDATION_EXPORT const unsigned char ARVideoKitLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARVideoKitLib/PublicHeader.h>
+
+
