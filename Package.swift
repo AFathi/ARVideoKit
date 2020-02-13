2eef6c5b44433fe0c329accb407bfe38efc29041
Giacomo <giacomo@jalvis.com>
2020-02-13 11:50:54 +0200

    Swift package manager

--- Package.swift
+++ Package.swift
@@ -1,8 +1,24 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "ARVideoKit"
+    name: "ARVideoKit",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "ARVideoKit",
+            targets: ["ARVideoKit"]),
+    ],
+    dependencies: [ ],
+    targets: [
+        .target(
+            name: "ARVideoKit",
+            dependencies: [],
+            path: "ARVideoKit"),
+    ]
 )
