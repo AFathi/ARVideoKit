2c1a98a12ab03320b1ea3c042ae22187efef8295
Giacomo <giacomo@jalvis.com>
2020-02-13 12:09:05 +0200

    Dynamic library

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ARVideoKit",
+            type: .dynamic,
             targets: ["ARVideoKit"]),
     ],
     dependencies: [ ],
@@ -19,6 +20,6 @@ let package = Package(
         .target(
             name: "ARVideoKit",
             dependencies: [],
-            path: "ARVideoKit"),
+            path: "ARVideoKit")
     ]
 )
