c6b94a485a09b240b127031dc9f157f0208312bd
Giacomo <giacomo@jalvis.com>
2020-02-26 16:26:20 +0100

    SwiftPM

--- Package.swift
+++ Package.swift
@@ -9,10 +9,8 @@ let package = Package(
         .iOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ARVideoKit",
-            type: .dynamic,
             targets: ["ARVideoKit"]),
     ],
     dependencies: [ ],
